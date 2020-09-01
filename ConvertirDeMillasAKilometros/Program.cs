818d94a9fb10087b6389fdd81026e334039b0fdd
Cristhian De Castro <cristhiancastro01@gmail.com>
2020-08-31 21:45:47 -0400

    Agregar archivos de proyecto.

---
+++ ConvertirDeMillasAKilometros/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConvertirDeMillasAKilometros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserte la cantidad de millas que desea convertir a Kilometros: ");
+           string valor1 = Console.ReadLine();
+
+            int millas1 = Convert.ToInt32(valor1);
+            double akilometros = millas1 * 1.609;
+
+            Console.WriteLine("{0} Millas = a {1} Kilometros", millas1, akilometros);
+
+        }
+    }
+}
